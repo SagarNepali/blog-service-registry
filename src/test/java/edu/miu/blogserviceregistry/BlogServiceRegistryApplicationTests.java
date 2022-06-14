dd872f645368a077544ad27965cb005afa6a8a9f
Sagar <worthless.dumboo@gmail.com>
2022-06-14 16:21:03 -0500

    Eureka server configurations

---
+++ src/test/java/edu/miu/blogserviceregistry/BlogServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.blogserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
